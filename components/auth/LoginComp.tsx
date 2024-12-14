1180b36d20b60dc38aea0747c21d9af0ba641008
ukonulucky <ukonulucky@gmail.com>
2024-12-14 22:41:02 +0100

    first ui update

--- components/auth/LoginComp.tsx
+++ components/auth/LoginComp.tsx
@@ -1,4 +1,5 @@
-import React, {useState } from "react";
+"use client"
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import Link from "next/link";
