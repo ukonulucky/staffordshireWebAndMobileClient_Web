a0471f0c63aa892183fb2dde954645563c9e3d9c
ukonulucky <ukonulucky@gmail.com>
2024-12-14 21:53:21 +0100

    first ui update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,  // Disable ESLint checks during the build
+  },
 };
 
+
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,  // Disable ESLint checks during the build
+  },
+};
 export default nextConfig;
